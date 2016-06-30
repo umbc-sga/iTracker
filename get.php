87efaedb1b5dc1dafce1226edca909fcfe7a916f
mlanden <mlanden@linux2.gl.umbc.edu>
2016-06-29 23:16:19 -0400

    Worked timeline and person

--- get.php
+++ get.php
@@ -1,32 +1,13 @@
 <?php
 
-/**
- * CONFIGURATION
- *
- * Configure these options with your basecamp details
- */
-
-// Replace with your basecamp user id that you can see in your basecamp url's https://basecamp.com/1234567 when visiting basecamp in the browser
-$basecampUserId = '2979808';
-
 // Username and Password of a basecamp user you wish to connect with
 include "/afs/umbc.edu/public/web/sites/sga/dev/cgi-bin/bc_cred.php";
-
-/**
- * SAMPLE GET SCRIPT THAT USES PHP CURL
- *
- * No need to change anything beyond this line...
- *
- * @link https://github.com/37signals/bcx-api
- */
 
 $url = $_GET['url'];
 
 if (empty($url)) {
     $url = 'projects.json';
 }
-
-$apiUrl = 'https://basecamp.com/' . $basecampUserId . '/api/v1/' . $url;
 
 // jSON String for request
 $json_string = '';
@@ -36,6 +17,7 @@ $ch = curl_init($apiUrl);
 
 // Configuring curl options
 $options = array(
+    CURLOPT_URL            => 'https://basecamp.com/2979808/api/v1/' . $url,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_USERPWD        => $credentials, // authentication; from included file bc_cred.php
     CURLOPT_HTTPHEADER     => array('Content-type: application/json'),
@@ -77,4 +59,5 @@ if ($result === false) {
     echo $result;
 }
 
+// Close open connection
 curl_close($ch);
