b695ca3b5534180b7d0854bf0fb9d882c1e1795f
sieabah <christopher.sidell@gmail.com>
2016-11-27 05:40:19 -0500

    Add name to footer

--- resources/views/templates/umbc/footer.blade.php
+++ resources/views/templates/umbc/footer.blade.php
@@ -9,7 +9,10 @@
             Follow UMBC: <a href="https://www.facebook.com/umbcpage" id="umbc-footer-facebook" title="Follow on Facebook">Facebook</a>, <a href="http://twitter.com/umbc" id="umbc-footer-twitter" title="Follow on Twitter">Twitter</a>, <a href="http://umbc.edu/news" id="umbc-footer-news" title="UMBC News">UMBC News</a>
         </nav>
     </div>
-    Created by <a href="https://www.linkedin.com/in/justinlchavez">Justin Chavez</a>, <a href="https://www.linkedin.com/in/matthew-landen-20a809100">Matthew Landen</a>, and <a href="https://www.linkedin.com/in/joshuamassey1">Joshua Massey</a>.<br/>
+    Created by <a target="_blank" href="https://christophersidell.com/">Christopher Sidell</a>,
+        <a target="_blank" href="https://www.linkedin.com/in/justinlchavez">Justin Chavez</a>,
+        <a target="_blank" href="https://www.linkedin.com/in/matthew-landen-20a809100">Matthew Landen</a>,
+        and <a target="_blank" href="https://www.linkedin.com/in/joshuamassey1">Joshua Massey</a>.<br/>
     Base Theme: AdminLTE by <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.
     <div id="umbc-footer-info">
         <strong>&copy; University of Maryland, Baltimore County</strong> • 1000 Hilltop Circle • Baltimore, MD 21250
