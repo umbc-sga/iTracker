302077ef9a0956185be30ac5a3b0fa3e908c3c50
sieabah <christopher.sidell@gmail.com>
2016-11-27 22:39:34 -0500

    Add loading spinner

--- resources/views/angular/allPeople.blade.php
+++ resources/views/angular/allPeople.blade.php
@@ -1,4 +1,6 @@
 <input type="text" ng-model="search.name" class="form-control" placeholder="Filter by person name" />
+
+<div class="loader smallLoader" ng-show="people.length <= 0"></div>
 <div class="row" style="margin-top:20px; display: flex; flex-wrap: wrap; align-items: stretch">
     <div class="col-xs-12 col-sm-4 col-lg-3" ng-repeat="person in people | filter: search | orderBy : 'name'">
         <div class="personView" data-person="person"></div>
