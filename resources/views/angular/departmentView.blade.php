ce63200cf6bfcf896cfff669185fc61360b4ccd0
sieabah <christopher.sidell@gmail.com>
2016-11-28 21:48:00 -0500

    Fix text decoration

--- resources/views/angular/departmentView.blade.php
+++ resources/views/angular/departmentView.blade.php
@@ -1,6 +1,8 @@
 <h2 id = "@{{department.id}}"> @{{department.name}}</h2>
 <div class="row" style="display: flex; flex-wrap: wrap; align-items: stretch">
     <div class="col-xs-12 col-sm-4 col-lg-3" ng-repeat="proj in department.projects | orderBy: 'name' | limitTo:5">
-        <div class="projectView" data-project="proj"></div>
+        <a data-ng-href="{{url('/project')}}/@{{ proj.id }}" style="text-decoration: none">
+            <div class="projectView" data-project="proj"></div>
+        </a>
     </div>
 </div>
