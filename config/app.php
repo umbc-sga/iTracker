1c502c820f48b8791d792645019a01b4569b5574
sieabah <christopher.sidell@gmail.com>
2016-12-03 03:58:31 -0500

    Remove Entrust

--- config/app.php
+++ config/app.php
@@ -189,7 +189,6 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Laravel\Socialite\SocialiteServiceProvider::class,
-        Zizaco\Entrust\EntrustServiceProvider::class,
 
         \App\Providers\BasecampProvider::class,
         \App\Providers\BasecampAPIProvider::class,
@@ -244,7 +243,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
-        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
 
     ],
 
