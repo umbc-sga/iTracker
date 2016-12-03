08ada84b0c80e6a39fd3bfb644edf0af2416ca06
sieabah <christopher.sidell@gmail.com>
2016-12-02 20:21:14 -0500

    Create organization tables

---
+++ database/migrations/2016_12_03_010649_make_user_org_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeUserOrgTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_organization', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('organization_id')->unsigned();
+            $table->integer('organization_role')->unsigned()->nullable();
+            $table->string('title', 200);
+            $table->timestamps();
+
+            $table->unique(['user_id', 'organization_id', 'organization_role'], 'unique_user_org_role');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_organization', function(Blueprint $table){
+            $table->dropUnique('unique_user_org_role');
+        });
+        Schema::dropIfExists('user_organization');
+    }
+}
