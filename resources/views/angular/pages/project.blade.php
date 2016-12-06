ecc1726d555e07de2cf474913708305f68d9a818
sieabah <christopher.sidell@gmail.com>
2016-12-05 20:39:55 -0500

    Fixes issue #3, fix breadcrumbs

--- resources/views/angular/pages/project.blade.php
+++ resources/views/angular/pages/project.blade.php
@@ -1,12 +1,4 @@
 
 <div class="content-wrapper">
-    <section class="content-header">
-        <ol class="breadcrumb">
-            <li><a href="{{url('/')}}"><i class="fa fa-home"></i> Home</a></li>
-            <li><a href="{{url('/projects/by-name/')}}">Projects</a></li>
-            <li class="active">@{{project.name}}</li>
-        </ol>
-    </section>
-
-    <section class="content project" data-token="{{csrf_token()}}"></section>
+    <section class="project" data-token="{{csrf_token()}}"></section>
 </div>
--- resources/views/angular/proj/project.blade.php
+++ resources/views/angular/proj/project.blade.php
@@ -1,147 +1,156 @@
-<h1>
-    @{{project.name}}
-</h1>
+<section class="content-header">
+    <h1>
+        @{{project.name}}
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="{{url('/')}}"><i class="fa fa-home"></i> Home</a></li>
+        <li><a href="{{url('/projects')}}">Projects</a></li>
+        <li class="active">@{{project.name}}</li>
+    </ol>
+</section>
 
-<div class="loader smallLoader" data-ng-show="!loaded"></div>
+<section class="content">
+    <div class="loader smallLoader" data-ng-show="!loaded"></div>
 
-<div class="row" data-ng-show="loaded">
-    <div class="col-xs-12">
-        <div class="col-xs-12 col-md-6">
-            <div class="box box-primary" data-ng-show="project.picture || canEdit">
-                <div data-ng-if="canEdit" class="hidden">
-                    <form data-ng-submit="">
-                        <input type="file" name="image" custom-on-change="uploadImage" />
-                    </form>
+    <div class="row" data-ng-show="loaded">
+        <div class="col-xs-12">
+            <div class="col-xs-12 col-md-6">
+                <div class="box box-primary" data-ng-show="project.picture || canEdit">
+                    <div data-ng-if="canEdit" class="hidden">
+                        <form data-ng-submit="">
+                            <input type="file" name="image" custom-on-change="uploadImage" />
+                        </form>
+                    </div>
+                    <div data-ng-show="project.picture">
+                        <img data-ng-src="@{{ project.picture }}"
+                             class="projectPicture"
+                             data-ng-class="{canEdit: canEdit}"
+                             data-ng-click="upload()"
+                             title="project picture"
+                             alt="project picture"
+                             width="100%"
+                             height="100%" />
+                    </div>
+                    <div data-ng-show="!project.picture && canEdit" style="cursor: pointer">
+                        <img src="//placeholdit.imgix.net/~text?txtsize=33&txt=upload&w=350&h=150"
+                             class="projectPicture"
+                             data-ng-class="{canEdit: canEdit}"
+                             data-ng-click="upload()"
+                             title="project picture"
+                             alt="project picture"
+                             width="100%"
+                             height="100%" />
+                    </div>
                 </div>
-                <div data-ng-show="project.picture">
-                    <img data-ng-src="@{{ project.picture }}"
-                         class="projectPicture"
-                         data-ng-class="{canEdit: canEdit}"
-                         data-ng-click="upload()"
-                         title="project picture"
-                         alt="project picture"
-                         width="100%"
-                         height="100%" />
-                </div>
-                <div data-ng-show="!project.picture && canEdit" style="cursor: pointer">
-                    <img src="//placeholdit.imgix.net/~text?txtsize=33&txt=upload&w=350&h=150"
-                         class="projectPicture"
-                         data-ng-class="{canEdit: canEdit}"
-                         data-ng-click="upload()"
-                         title="project picture"
-                         alt="project picture"
-                         width="100%"
-                         height="100%" />
-                </div>
-            </div>
-            <div class="box box-primary" data-ng-show="project.ratio >= 0">
-                <div class="box-header with-border">
-                    <h3 class="box-title">
-                        Progress Bar
-                    </h3>
-                </div>
-                <div class="box-body">
-                    <div class="progress progress-xl progress-striped active" style="height:30px;">
-                        <div class="progress-bar progress-bar-primary"
-                             style="width: @{{ project.ratio }}%">
-                            <h6>@{{ project.ratio }}%</h6>
+                <div class="box box-primary" data-ng-show="project.ratio >= 0">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">
+                            Progress Bar
+                        </h3>
+                    </div>
+                    <div class="box-body">
+                        <div class="progress progress-xl progress-striped active" style="height:30px;">
+                            <div class="progress-bar progress-bar-primary"
+                                 style="width: @{{ project.ratio }}%">
+                                <h6>@{{ project.ratio }}%</h6>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div class="meetTheTeam" data-members="project.people" data-team-title="People on this Project"></div>
+                @if(config('services.basecamp.openAccess'))
+                    <div class="box-footer">
+                        <form action="{{route('project.join')}}" method="post" style="display:inline;">
+                            <input type="hidden" name="_method" value="PUT">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="projectID" value="@{{project.id}}">
+                            <button type="submit" class="btn btn-primary btn-block btn-flat ">Join the team!</button>
+                        </form>
+                    </div>
+                @endif
             </div>
-            <div class="meetTheTeam" data-members="project.people" data-team-title="People on this Project"></div>
-            @if(config('services.basecamp.openAccess'))
-                <div class="box-footer">
-                    <form action="{{route('project.join')}}" method="post" style="display:inline;">
-                        <input type="hidden" name="_method" value="PUT">
-                        {{ csrf_field() }}
-                        <input type="hidden" name="projectID" value="@{{project.id}}">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat ">Join the team!</button>
-                    </form>
+            <div class="col-xs-12 col-md-6">
+                <div class="box box-primary">
+                    <div class="row">
+                        <div class="col-xs-12 projectAtAGlance"
+                             data-project="project"
+                             data-read-more="false"></div>
+                    </div>
                 </div>
-            @endif
+
+                <div class="loader smallLoader" data-ng-show="!eventsLoaded"></div>
+                <div class="box box-primary col-xs-12" data-ng-show="eventsLoaded">
+                    <h2>Project Events</h2>
+                    <div class="box-body" data-ng-show="project.events.length <= 0">
+                        <h3><small>(No history)</small> </h3>
+                    </div>
+                    <div data-ng-repeat="event in project.events">
+                        <div class="basecampEvent" data-event="event"></div>
+                    </div>
+                </div>
+
+                <div class="loader smallLoader" data-ng-show="!historyLoaded"></div>
+                <div class="box box-primary col-xs-12" data-ng-show="historyLoaded">
+                    <h2>History of the Project</h2>
+                    <div class="box-body" data-ng-show="project.history.length <= 0">
+                        <h3><small>(No history)</small> </h3>
+                    </div>
+                    <div class="iTrakertimeline" data-timeline="project.history" data-ng-show="project.history.length > 0"></div>
+                </div>
+
+                <div class="loader smallLoader" data-ng-show="!todoLoaded"></div>
+                <div class="box box-primary col-xs-12" data-ng-show="todoLoaded">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">To-Do Lists <small>(Click on list name to expand)</small> </h3>
+                    </div>
+                    <div class="box-body" data-ng-show="project.todo.length <= 0">
+                        <h3><small>(There are no todolists)</small> </h3>
+                    </div>
+                    <div class="projectTodo" data-todos="project.todo" data-ng-show="project.todo.length > 0"></div>
+                </div>
+            </div>
         </div>
-        <div class="col-xs-12 col-md-6">
-            <div class="box box-primary">
-                <div class="row">
-                    <div class="col-xs-12 projectAtAGlance"
-                         data-project="project"
-                         data-read-more="false"></div>
-                </div>
-            </div>
 
-            <div class="loader smallLoader" data-ng-show="!eventsLoaded"></div>
-            <div class="box box-primary col-xs-12" data-ng-show="eventsLoaded">
-                <h2>Project Events</h2>
-                <div class="box-body" data-ng-show="project.events.length <= 0">
-                    <h3><small>(No history)</small> </h3>
-                </div>
-                <div data-ng-repeat="event in project.events">
-                    <div class="basecampEvent" data-event="event"></div>
-                </div>
-            </div>
-
-            <div class="loader smallLoader" data-ng-show="!historyLoaded"></div>
-            <div class="box box-primary col-xs-12" data-ng-show="historyLoaded">
-                <h2>History of the Project</h2>
-                <div class="box-body" data-ng-show="project.history.length <= 0">
-                    <h3><small>(No history)</small> </h3>
-                </div>
-                <div class="iTrakertimeline" data-timeline="project.history" data-ng-show="project.history.length > 0"></div>
-            </div>
-
-            <div class="loader smallLoader" data-ng-show="!todoLoaded"></div>
-            <div class="box box-primary col-xs-12" data-ng-show="todoLoaded">
+        {{--
+        <div class="col-xs-12">
+            <div class="box box-primary" ng-show="project.attachments.count > 0">
                 <div class="box-header with-border">
-                    <h3 class="box-title">To-Do Lists <small>(Click on list name to expand)</small> </h3>
+                    <h3 class="box-title">Documents</h3>
                 </div>
-                <div class="box-body" data-ng-show="project.todo.length <= 0">
-                    <h3><small>(There are no todolists)</small> </h3>
+                <div class="box-body">
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>Document</th>
+                            <th>Added By</th>
+                            <th></th>
+                        </tr>
+                        <tr ng-repeat="doc in attachments">
+                            <td valign="middle">
+                                <h5>@{{doc.name}}</h5>
+                                <h6>Created: @{{doc.created_at | date}} | Last update: @{{doc.updated_at | date}}</h6>
+                            </td>
+                            <td align="center" valign="middle">
+                                <a href="/itracker/people/@{{doc.creator.id}}/">
+                                    <img src="@{{doc.creator.avatar_url}}" class="img-circle" title="@{{doc.creator.name}}" alt="@{{doc.creator.name}}">
+                                    <h5>@{{doc.creator.name}}</h5>
+                                </a>
+                            </td>
+                            <td>
+                                <h5>@{{doc.content_type}}</h5>
+                                <a class="btn btn-app" ng-show="doc.link_url != null" href="@{{doc.link_url}}">
+                                    <i class="fa" ng-class="{'fa-file-image-o':docType(doc.content_type), 'fa-refresh': !docType(doc.content_type)}"></i>
+                                    <i class="fa @{{getClass(doc.content_type)}}" ></i>
+                                    <h5>Download</h5>
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
-                <div class="projectTodo" data-todos="project.todo" data-ng-show="project.todo.length > 0"></div>
             </div>
         </div>
-    </div>
-
-    {{--
-    <div class="col-xs-12">
-        <div class="box box-primary" ng-show="project.attachments.count > 0">
-            <div class="box-header with-border">
-                <h3 class="box-title">Documents</h3>
-            </div>
-            <div class="box-body">
-                <table class="table table-bordered">
-                    <tr>
-                        <th>Document</th>
-                        <th>Added By</th>
-                        <th></th>
-                    </tr>
-                    <tr ng-repeat="doc in attachments">
-                        <td valign="middle">
-                            <h5>@{{doc.name}}</h5>
-                            <h6>Created: @{{doc.created_at | date}} | Last update: @{{doc.updated_at | date}}</h6>
-                        </td>
-                        <td align="center" valign="middle">
-                            <a href="/itracker/people/@{{doc.creator.id}}/">
-                                <img src="@{{doc.creator.avatar_url}}" class="img-circle" title="@{{doc.creator.name}}" alt="@{{doc.creator.name}}">
-                                <h5>@{{doc.creator.name}}</h5>
-                            </a>
-                        </td>
-                        <td>
-                            <h5>@{{doc.content_type}}</h5>
-                            <a class="btn btn-app" ng-show="doc.link_url != null" href="@{{doc.link_url}}">
-                                <i class="fa" ng-class="{'fa-file-image-o':docType(doc.content_type), 'fa-refresh': !docType(doc.content_type)}"></i>
-                                <i class="fa @{{getClass(doc.content_type)}}" ></i>
-                                <h5>Download</h5>
-                            </a>
-                        </td>
-                    </tr>
-                </table>
-            </div>
+        <div class="col-md-7">
+            <div class="iTrakertimeline" data-timeline="events"></div>
         </div>
+        --}}
     </div>
-    <div class="col-md-7">
-        <div class="iTrakertimeline" data-timeline="events"></div>
-    </div>
-    --}}
 </div>
